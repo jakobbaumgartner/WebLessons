49663e67af813d9fdcc8d3537d18b7556613a16d
JB <jakob.baumgartner@hotmail.com>
2020-03-16 20:29:00 +0100

    flexbox

--- weblessons/src/Components/Main/MainPrimary.js
+++ weblessons/src/Components/Main/MainPrimary.js
@@ -21,14 +21,18 @@ export default class MainPrimary extends React.Component {
         if (document.documentElement.clientHeight * 0.6 * 16 / 9 < document.documentElement.clientWidth * 0.6) {
             this.player.style.height = String(document.documentElement.clientHeight * 0.6) + 'px'
             this.player.style.width = String(document.documentElement.clientHeight * 0.6 * 16 / 9) + 'px'
+            console.log(document.documentElement.clientWidth - this.player.offsetWidth)
+            // document.getElementById('secondary').style.width = String((document.documentElement.clientWidth - document.getElementById('primary').offsetWidth)*0.9) + 'px'
+            // document.getElementById('primary').style.width = String((this.player.clientWidth*1.05)) + 'px'
         }
 
         else {
             this.player.style.height = String(document.documentElement.clientWidth * 0.6 * 9/16) + 'px'
             this.player.style.width = String(document.documentElement.clientWidth * 0.6) + 'px'
-
-        }
+            // document.getElementById('secondary').style.width = String((document.documentElement.clientWidth - document.getElementById('primary').offsetWidth)*0.9) + 'px'
+            // document.getElementById('primary').style.width = String((this.player.clientWidth *1.05)) + 'px'        
         // this.opts.height = String((this.player.offsetWidth)*9/16) + 'px'
+        }
 
     }
 
---
+++ weblessons/src/Components/Main/MainSecundary.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export default class MainSecundary extends React.Component {
+
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div id='chat'>
+                <p>ff</p>
+            </div>
+
+        )
+    }
+
+}
